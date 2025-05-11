1acefc081e2169bebdf9b2350d28f7b362d2fe11
Maciej <78358786+cjrnd@users.noreply.github.com>
2025-05-12 00:17:59 +0200

    csv file generation fixed

--- QCMMeasurements/visa_read_data.py
+++ QCMMeasurements/visa_read_data.py
@@ -3,39 +3,47 @@ from time import sleep
 from quantiphy import Quantity
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
+import pandas as pd
+import time
 
 #pyvisa.log_to_screen()
 rm = pyvisa.ResourceManager('@py')
-print(rm.list_resources())
-dmm = rm.open_resource('USB0::10893::5121::MY57217967::0::INSTR')
+rscs = rm.list_resources('USB*')
+print(rscs)
+dmm = rm.open_resource(rscs[0])
+#dmm = rm.open_resource('USB0::10893::5121::MY57217967::0::INSTR')
 print(dmm.query("*IDN?"))
 
-
+dmm.write("*RST")
+dmm.write("*CLS")
+#dmm.write(':FREQ:UNIT HZ')
+#dmm.write(':FREQ:MEAS:TIME 5') 
 
 N = 5
 i = 0
 
 meas_number=[]
 meas_value=[]
-
-while i < N:
+start = time.time()
+for i in range(N):
     #print("Pomiar", i, ":", Quantity(dmm.query("MEAS:VOLT:AC?").strip()),"VAC")
     meas_number.append(i+1)
-    meas_value.append(float(Quantity(dmm.query(":MEAS:VOLT:AC? 10").strip())))
-    plt.plot(meas_number, meas_value, linestyle='-', marker='o')
-    #plt.scatter(meas_number, meas_value)
-    plt.pause(0.01)
-    sleep(0.01)
-    i += 1
+    meas_value.append(float(Quantity(dmm.query(":MEAS:FREQ?").strip())))
+    #sleep(0.1)
+dmm.close()
+end = time.time()
+print("Pomiar trwal:", end - start, "s")
 
-print(meas_number)
-print(meas_value)
+plt.plot(meas_number, meas_value, linestyle='-', marker='o')
 
-#with open('pomiary.csv', 'w', newline='') as file:
- #   writer = csv.writer(file)
-  #  header = ["Numer pomiaru", "Wartość zmierzona"]
-   # writer.writerow(header)
+joined_tables = pd.DataFrame({
+    'Pomiar': meas_number,
+    'Wartosc': meas_value
+})
+
+print(joined_tables)
+joined_tables.to_csv('measures.csv', index=False)
+print("CSV SAVED")
 
 plt.tight_layout()
 plt.grid()
